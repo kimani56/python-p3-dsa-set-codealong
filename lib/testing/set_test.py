569406e3cf3f3a2df32e0920131ab152b2acf85e
gift kimani <gift.kimani@students.moringaschool.com>
2023-09-11 20:53:25 +0300

    complete

--- lib/testing/set_test.py
+++ lib/testing/set_test.py
@@ -39,8 +39,8 @@ class TestSet:
 
 
 
-# Bonus test
-"""
+
+
     def test_clear(self):
         '''Test clearing set'''
         test_set = MySet([1,2,3,4])
@@ -51,4 +51,3 @@ class TestSet:
         '''Test __str__()'''
         test_set = MySet([1,2,3,4])
         assert(str(test_set) == 'MySet: {1,2,3,4}')
-"""
